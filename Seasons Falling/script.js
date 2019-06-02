89ff629483efe3413ae1a01abe817f7f063ad337
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-02 21:18:02 +0200

    wow, it worked at first try😦

--- Seasons Falling/script.js
+++ Seasons Falling/script.js
@@ -3,3 +3,33 @@
  * Ramon Morcillo @reymon359
  * ramonmorcillo.com
  */
+
+function changeSeason() {
+    let html = '';
+    // I will build the html here
+    html += `<section><h2>Seasons Falling</h2>`;
+
+    for (let containerIndex = 1; containerIndex < 4; containerIndex++) {
+        // Now I make 3 set containers
+        html += `<div class="set set${containerIndex}">`;
+
+        // now 3 groups of 3 images each
+        for (let imgGroupIndex = 1; imgGroupIndex < 4; imgGroupIndex++) { // The loop for groups
+
+            for (let imgIndex = 1; imgIndex < 4; imgIndex++) { // The loop for the images
+
+                html += `<div><img src="https://raw.githubusercontent.com/reymon359/web-experiments/master/Seasons%20Falling/season1_${imgIndex}.png" alt=""></div>`;
+
+            }
+        }
+
+        html += `</div>`;
+    }
+
+
+
+    html += `</section>`;
+    console.log(html);
+    document.getElementsByTagName('body')[0].innerHTML = html;
+
+}
