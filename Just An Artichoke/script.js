cacf4762832bde9171eaa1bf110100b5f115c5fb
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-17 20:42:17 +0200

    gltf loader

--- Just An Artichoke/script.js
+++ Just An Artichoke/script.js
@@ -18,15 +18,32 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementsByTagName('body')[0].append(renderer.domElement);
 
 // Now we will create a json loader object
-const obj;
-const loader = new THREE.JSONLoader();
+// let obj;
+// const loader = new THREE.LegacyJSONLoader();
+// // The function receives the geometry 
+// loader.load('artichoke.json', (g, m) => {
+//     // We will create a new mesh using the loaded object and the material
+//     obj = new THREE.Mesh(g, m);
+//     scene.add(obj);
 
+//     // Now we place the object
+
+// })
+
+// Ambient light
+let light1 = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light1);
+
+// Point light
+let light2 = new THREE.PointLight(0xffffff, 0.5);
+scene.add(light2);
 
 // By default, when we call scene.add(), the thing we add will be added to the coordinates (0,0,0). 
 // This would cause both the camera and the cube to be inside each other. 
 // To avoid this, we simply move the camera out a bit.
 camera.position.z = 5;
-cube.material.color.setHex('0x' + Math.floor(Math.random() * 16777215).toString(16));
+
+
 
 // Render or animate loop
 function animate() {
@@ -34,11 +51,11 @@ function animate() {
     requestAnimationFrame(animate);
 
     // Rotation stuff
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
-    // Sabroseo sabroso
-    if (Math.floor(Math.random() * 100) < 2) cube.material.color.setHex('0x' + Math.floor(Math.random() * 16777215).toString(16));
+    // // Sabroseo sabroso
+    // if (Math.floor(Math.random() * 100) < 2) cube.material.color.setHex('0x' + Math.floor(Math.random() * 16777215).toString(16));
 
 
     // Rendering it
